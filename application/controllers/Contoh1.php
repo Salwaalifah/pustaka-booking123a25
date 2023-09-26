38aa3379aef42c263e28b664e6db4f90d9c85346
salwaa <salifah54@gmail.com>
2023-09-26 20:47:03 +0700

    Membuat project Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index(){
+        echo "<h1> Perkenalan diri </h1>";
+        echo "Nama saya Shalwa
+        Saya tinggal di Switzerland
+        Saya sangat suka melukis dan menonton film";
+    }
+
+}
+?>
